fa9ca56ac5dcfe3fd155e7d3e4807be56fa6b863
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-01-29 09:54:01 -0700

    I worked on prints and inputs

---
+++ Variables/practice.py
@@ -0,0 +1,8 @@
+# Ryan Crop, Variables Practice Python
+
+Name = ("My name is Ryan Crop")
+print(Name)
+Small_Number = input("Pick a number 1-10")
+print(Small_Number,6 "Is a nice choice nice choice")
+Big_Number =input("Now chose a number 100-1000")
+print(Big_Number, "Is also a great choice")
