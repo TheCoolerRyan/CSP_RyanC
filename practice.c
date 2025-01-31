27fd1778db78d2b1e69c30f7841bb705618e44c7
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-01-31 09:22:17 -0700

    Did the C practice

---
+++ practice.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+char name [] = "Ryan Crop";
+int small_num = 9;
+int big_num = 107;
+char breakfeast [] = "eggs and bacon";
+char color [] = "Red";
+char school [] = "UCAS";
+int year = 2025;
+char eye [] = "Blue";
+int age = 15;
+char favroite_subject [] = "Coding";
+int main(void){
+    printf(name);
+    printf("\n",name);
+    printf("%d\n",small_num);
+    printf("%d\n",big_num);
+    printf("%s\n",breakfeast);
+    printf("%s\n",color);
+    printf("%s\n",school);
+    printf("%d\n",year);
+    printf("%s\n",eye);
+    printf("%d\n",age);
+    printf("%s\n",favroite_subject);
+     return 0;
+}
