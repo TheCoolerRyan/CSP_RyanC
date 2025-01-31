7e277a699ec114658861aa8dd44816e592703228
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-01-31 09:48:49 -0700

    Finished Program C

---
+++ Variables/program.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char name[] = "person";
+int num = 42;
+float pi = 3.14;
+int mynum;
+int my;
+int main(void){
+    printf("Hello %s, my age is %d and my favroite number is %f\n", name, num, pi);
+    printf("type a number but don't tell me it: \n");
+    scanf("%d", &mynum);
+    printf("Your magic number is: %d", mynum);
+    printf("\n Thats a cool one");
+    return 0;
+}
