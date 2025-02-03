5c38d3fd0ed450ca7aa736f11ada2364fb2f6a5f
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-02-03 09:22:11 -0700

    Fixed template for C

--- Variables/template.c
+++ Variables/template.c
@@ -1,12 +1,7 @@
+//Ryan Crop, Template
 #include <stdio.h>
 
-char name[] = "Crop";
-int num = 42;
-float pi = 3.14;
-
 int main(void){
-    printf("Hello %s, my age is %d and my favroite number is %f\n", name, num, pi);
-    printf("%d\n", num);
-    printf("%f\n", pi);
+printf("Hello World");
     return 0;
 }
