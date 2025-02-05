9f0f6b4346ddfbc6a7b569b960ef92ede6af762e
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-02-05 19:47:24 +0000

    Financial calculator

--- Expressions/finacil_calculator.py
+++ Expressions/finacil_calculator.py
@@ -1,2 +1,14 @@
-money = input("How much money do you make a year?")
-if money <9000 else print("thats great.")
+#Ryan Crop, Finaincal calculator.
+money = input("How much money do you make this year?")
+print("Okay")
+loss = input("How much money did you spend this year?")
+print("Got it.")
+if money > loss:
+    print("Thats great!")
+if money < loss:
+    print("Will need to work on that.")
+    rent = input("How much did you spend on Rent?")
+    utilites = input("Okay, how much did you spend on utilites?")
+    grocies = input("Got it, so how much did you spend on grocies?")
+    transportation =input("And finally how much did you spend on transportation?")
+    # Add up
