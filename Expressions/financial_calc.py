0f8355a06fa3f26a0c32bbbf42e2c97622f4b7e5
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-02-06 19:46:30 +0000

    Finished finacial calculator

--- Expressions/financial_calc.py
+++ Expressions/financial_calc.py
@@ -22,9 +22,9 @@
 
 #Calculate percent of grocieries (groceries/income) -
 
-#Calculate percent of transportation (transportation/income)
+#Calculate percent of transportation (transportation/income)-
 
-# Calculate percent of spending (spending/income)
+# Calculate percent of spending (spending/income)-
 
 # tell user category spending amount and percent ("You spend $xx.xx on rent and that is xx% of your income") for all
 
@@ -37,8 +37,19 @@ utilites = float(input("Okay, how much did you spend on utilites?"))
 grociries = float(input("Got it, so how much did you spend on grocies?"))
 transportation = float(input("And finally how much did you spend on transportation?"))
 savings = float(income/90)
-spending_money = (income - (rent+utilites+grociries+transportation+savings))
-percent_rent = (rent/income)
-percent_utilites = (utilites/income)
-percent_grociries = (grociries/income)
-
+spending_money = float(income - (rent+utilites+grociries+transportation+savings))
+percent_rent = float(rent/income)
+percent_utilites = float(utilites/income)
+percent_grociries = float(grociries/income)
+percent_transportation = float(transportation/income)
+percent_spending_money = float(spending_money/income)
+print("The amount you spend on rent is", rent) 
+print("and the percent of your income you spend on rent is",percent_rent)
+print("The amount you spend on grociries are", grociries) 
+print("and the percent of your income you spend on grociries is",percent_grociries)
+print("The amount you spend on utilities are", utilites) 
+print("and the percent of your income you spend on utilities is",percent_utilites)
+print("The amount you spend on transportation is", transportation) 
+print("and the percent of your income you spend on transportation is",percent_transportation)
+print("The amount of money you should save is",savings)
+print("Finally the amount of spending money you have to use is",spending_money)
