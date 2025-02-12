0c2616d5ef94bf28f8975738fa5205918f26b26d
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-02-12 09:13:01 -0700

    Finsished updating name decorator

---
+++ strings/name_decorator.c
@@ -0,0 +1,1 @@
+//Ryan Crop, Name Decorator 
--- strings/name_decorator.py
+++ strings/name_decorator.py
@@ -1,5 +1,6 @@
+#Ryan Crop, Name Decorator 
 name = input("What is your name?")
 smile1 = "== :P B ) ="
-smile2 = "= (< (B =="
+smile2 = "= q: ( B =="
 name = (smile1+name)
 print (name+smile2)
