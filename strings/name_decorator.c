976ff4518cd33bccfe4cc4dc823e6c0ebfbbca83
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-02-12 09:39:22 -0700

    Got rid of a \n in a scanf statement

--- strings/name_decorator.c
+++ strings/name_decorator.c
@@ -6,7 +6,7 @@ char bedazled1[20] = ("<-=-");
 char bedazled2[20] = ("-=->");
 int main(void){
 printf("Hello, what is your name?");
-scanf("%s\n", name);
+scanf("%s", name);
 strcat(bedazled1, name);
 strcat(bedazled1, bedazled2);
 printf("Behold your knew and imporved name is...%s", bedazled1);
