057b0715af9e9682b9006caf318d12222b5eb970
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-02-12 09:31:43 -0700

    Finished name decorator.c

--- strings/name_decorator.c
+++ strings/name_decorator.c
@@ -1,1 +1,15 @@
 //Ryan Crop, Name Decorator 
+#include <stdio.h>
+#include <string.h>
+char name [20];
+char bedazled1[20] = ("<-=-");
+char bedazled2[20] = ("-=->");
+int main(void){
+printf("Hello, what is your name?");
+scanf("%s\n", name);
+strcat(bedazled1, name);
+strcat(bedazled1, bedazled2);
+printf("Behold your knew and imporved name is...%s", bedazled1);
+printf("!!!");
+    return 0;
+}
