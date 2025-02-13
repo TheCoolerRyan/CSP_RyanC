0aa2e4c6f03ca99710b3acaeda620f42e9f06c21
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-02-13 09:53:31 -0700

    Silly_sentences.py

---
+++ strings/silly_sentences.py
@@ -0,0 +1,10 @@
+#Ryan Crop, Silly Sentences python
+name = input("What is your name?")
+print(f"Hello {name}, this program will take words that you give it and put them into a fun sentence.")
+verb = input("Please tell me a verb (Only the verb and have it end in ing)")
+place = input("Now tell me a place that you go to frequently.(Only the place)")
+adjective = input("Now could you tell me the first adjevtive that pops into your mind. (Only the adjective)")
+person = input("Finally, tell me the first name of someone you know. (Only the first name please.)")
+print("Your new and hilarious sentence is... ")
+print(f"You where {verb} in a {place} until you ran into a {adjective} {person}")
+#Finish the print statement right before this .
