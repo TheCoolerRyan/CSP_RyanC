15122db94043f2f4176e6d7d90d100f97f6cb446
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-02-21 09:16:31 -0700

    Finished Hello world :)

--- function/update_hello_world.c
+++ function/update_hello_world.c
@@ -1,19 +1,15 @@
 //Ryan Crop, update hello world program C
 #include <stdio.h>
+char user(char name[20]){
+    printf("Hello %s\n", name);
+}
 int main(void){
-printf("Hello World");
-
+printf("Hello World!\n");
+user("Julia!");
+user("Caden!");
+user("Reese!");
+user("Justin!");
+user("Rachel!");
     return 0;
 }
-char name1[50] = "James";
-char name2[50] = "Rachel";
-char name3[50] = "Justin";
-char name4[50] = "Caden";
-char name5[50] = "Reese";
-void input(char type[50], int length){
-printf("Hello %s\n", name1);
-printf("Hello %s\n", name2);
-printf("Hello %s\n", name3);
-printf("Hello %s\n", name4);
-printf("Hello %s\n", name5);
-}
+
