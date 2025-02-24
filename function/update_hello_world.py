2415a3dcdd94a7977d48c343bdafb1a380cddf21
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-02-24 03:34:26 +0000

    Updated my hello world so it only has one print statement.

--- function/update_hello_world.py
+++ function/update_hello_world.py
@@ -1,13 +1,10 @@
 #Ryan Crop, Update hello world python
 def user(name):
-    return input(f"Please tell me a {name}\n")
-name1 = user("name:")
-name2 = user("name:")
-name3 = user("name:")
-name4 = user("name:")
-name5 = user("name:")
-print(f"Hello {name1}")
-print(f"Hello {name2}")
-print(f"Hello {name3}")
-print(f"Hello {name4}")
-print(f"Hello {name5}")
+    print(f"Hello {name}")
+    name = {user}
+user(name ="Justin!")
+user(name ="caden!")
+user(name ="Julia!")
+user(name ="Reese!")
+user(name ="Rachel!")
+
