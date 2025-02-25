8cff30579e73e9a8fccecfa48fadd8dc5c3f162e
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-02-25 22:39:05 +0000

    Fixed old enough program

--- conditionals/old_enough.py
+++ conditionals/old_enough.py
@@ -1,11 +1,13 @@
 # Ryan Crop, Old enough python
-age = input("How old are you?")
+age = int(input("How old are you?\n"))
 if age >= 5:
-    print(f"Wow, {age} is old enough that you can go to school.\n")
+    print(f"Wow, {age} is old enough that you can go to school!\n")
+if age >= 15:
+    print(f"Wow being {age} means you can get a Learners permit!\n")
 else:
     print(f"Sorry but {age} is not old enough to go to school.\n")
 if age >= 15:
-    print(f"Wow being {age} means you can get a Learners permit.\n")
+    print(f"Wow being {age} means you can get a Learners permit!\n")
 else:
     print(f"Sorry but sense your only {age} you can't get your learners permit.\n")
 if age >=16:
@@ -13,7 +15,8 @@ if age >=16:
 else:
     print(f"Sorry but sense your only {age} you can't drive yet.")
 if age >= 18:
-    print(f"Wow because your {age} you can vote.")
+    print(f"Wow because your {age} you can vote!")
 else:
     print(f"Sorry but because your only {age} years old, you can't vote yet.")
     
+#Put highest age at the top and its else at the bottom.
