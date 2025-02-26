3399977e8164b77c02ffa5f3bc7a1735cab45a5f
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-02-26 09:54:07 -0700

    Old enough program

---
+++ conditionals/old_enough.c
@@ -0,0 +1,12 @@
+//Ryan Crop, old enough c
+#include <stdio.h>
+#include <string.h>
+age;
+int main(void){
+    printf("What is your age?");
+    scanf("%d", age);
+if (age >= 18 ){
+    printf("Wow because your %d you can vote!\n", age);
+}
+    return 0;
+}
--- conditionals/old_enough.py
+++ conditionals/old_enough.py
@@ -1,22 +1,19 @@
 # Ryan Crop, Old enough python
 age = int(input("How old are you?\n"))
 if age >= 18:
-    print(f"Wow because your {age} you can vote!\n")
-    if age >=16:
-        print(f"Sense your {age} that means you can drive!\n")
-        if age >= 15:
-            print(f"Wow being {age} means you can get a Learners permit!\n")
-            if age >= 5:
-                print(f"Wow, {age} is old enough that you can go to school!\n")
-            else:
-                print(f"Sorry but {age} is not old enough to go to school.\n")
-        else:
-            print(f"Sorry but sense your only {age} you can't get your learners permit.\n")
-
-    else:
-        print(f"Sorry but sense your only {age} you can't drive yet.\n")
-else:
-    print(f"Sorry but because your only {age} years old, you can't vote yet.\n")
-
-
-#Put highest age at the top and its else at the bottom.
+    print(f"Wow because your {age} you can vote, drive, get your learners permit, and go to school!\n") 
+elif age < 18 & age >16:
+    print(f"Sorry but because your only {age} years old, you can't vote, but you can drive, get your learners permit and go to school.\n")
+elif age >15 & age < 18:
+    print(f"Sense your {age} that means you can drive get your leanres permit and go to school but you cant vote!\n")
+elif age == 15:
+    print(f"Sorry but sense your only {age} you can't drive yet or vote, but you can get your learners permit and go to school.\n")
+elif age > 14 & age < 16:
+    print(f"Wow being {age} means you can get a Learners permit and go to school but you cant drive or vote!\n")
+elif age < 15 & age >5:
+    print(f"Sorry but sense your only {age} you can't get your learners permit drive or vote but you can go to school.\n")
+elif age >= 5 & age <15:
+    print(f"Wow, {age} is old enough that you can go to school, but you cant get your learners permit drive or vote!\n")
+elif age <5:
+    print(f"Sorry but {age} is not old enough to go to school, get your learners permit, drive, or vote.\n")
+# Put in else statement
