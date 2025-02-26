da0dadcbcbe5f65a15cb5fa62c5308da21b1ce89
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-02-26 09:16:58 -0700

    Finished old enough python

--- conditionals/old_enough.py
+++ conditionals/old_enough.py
@@ -1,22 +1,22 @@
 # Ryan Crop, Old enough python
 age = int(input("How old are you?\n"))
-if age >= 5:
-    print(f"Wow, {age} is old enough that you can go to school!\n")
-if age >= 15:
-    print(f"Wow being {age} means you can get a Learners permit!\n")
-else:
-    print(f"Sorry but {age} is not old enough to go to school.\n")
-if age >= 15:
-    print(f"Wow being {age} means you can get a Learners permit!\n")
-else:
-    print(f"Sorry but sense your only {age} you can't get your learners permit.\n")
-if age >=16:
-    print(f"Sense your {age} that means you can drive!\n")
-else:
-    print(f"Sorry but sense your only {age} you can't drive yet.")
 if age >= 18:
-    print(f"Wow because your {age} you can vote!")
+    print(f"Wow because your {age} you can vote!\n")
+    if age >=16:
+        print(f"Sense your {age} that means you can drive!\n")
+        if age >= 15:
+            print(f"Wow being {age} means you can get a Learners permit!\n")
+            if age >= 5:
+                print(f"Wow, {age} is old enough that you can go to school!\n")
+            else:
+                print(f"Sorry but {age} is not old enough to go to school.\n")
+        else:
+            print(f"Sorry but sense your only {age} you can't get your learners permit.\n")
+
+    else:
+        print(f"Sorry but sense your only {age} you can't drive yet.\n")
 else:
-    print(f"Sorry but because your only {age} years old, you can't vote yet.")
-    
+    print(f"Sorry but because your only {age} years old, you can't vote yet.\n")
+
+
 #Put highest age at the top and its else at the bottom.
