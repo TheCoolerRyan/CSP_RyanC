4aed2be3ff4d079ceaf9e06e47894dea0a3e5cfa
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-02-28 09:18:52 -0700

    Finished cleaning up update hello world

--- function/update_hello_world.py
+++ function/update_hello_world.py
@@ -1,10 +1,9 @@
 #Ryan Crop, Update hello world python
 def user(name):
     print(f"Hello {name}")
-    name = {user}
-user(name ="Justin!")
-user(name ="caden!")
-user(name ="Julia!")
-user(name ="Reese!")
-user(name ="Rachel!")
+user("Justin!")
+user("caden!")
+user("Julia!")
+user("Reese!")
+user("Rachel!")
 
