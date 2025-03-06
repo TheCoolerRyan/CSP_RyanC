afa8a2882db7bdf7d535669cbea3e5d265a6b6af
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-03-06 19:46:28 +0000

    Worked on fizzbuzz

---
+++ loops/fizzbuzz.c
@@ -0,0 +1,11 @@
+//Ryan Crop, Template
+#include <stdio.h>
+numbers = 0
+int main(void){
+while numbers >= 0 && numbers <51{
+    if(numbers % 15 == 0):
+    printf("FizzBuzz!");
+}
+
+    return 0;
+}
