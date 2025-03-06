c24ea6952e42f156192c82e32a14e27d4ae4a8f2
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-03-06 22:37:12 +0000

    Tried to fix Fizzbuzz

--- loops/fizzbuzz.c
+++ loops/fizzbuzz.c
@@ -1,11 +1,17 @@
 //Ryan Crop, Template
 #include <stdio.h>
-numbers = 0
+numbers = 0;
 int main(void){
-while numbers >= 0 && numbers <51{
-    if(numbers % 15 == 0):
+while (numbers >= 0 && numbers <51){
+    if(numbers % 15 == 0){
     printf("FizzBuzz!");
-}
-
+}else if(numbers % 5 == 0){
+    printf("Buzz.");
+}else if(numbers % 3 == 0){
+    printf("Fizz.");
+}else{
+    printf(numbers);
+}}
+    numbers += 1;
     return 0;
 }
