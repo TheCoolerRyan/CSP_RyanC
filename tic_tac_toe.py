b6fa31da883a2add62e23a2fe65bce5e70da5a60
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-03-19 18:46:15 +0000

    get rid of x function thingy

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,11 +1,12 @@
 # Ryan Crop, Final project
-x = 1 
-x = 3
-O = 0
+x = 0
+("x") == 52
+("O") == 46
+4 = ("x")
 import random
 random_integer = random.randint(1, 9)
-if random_integer == x or random_integer == O:
-    random_integer = random.randint
-    print(random_integer)
+if random_integer == 52 or random_integer == 46:
+    print("Hello")
 else:
-    print ("Hello")
+    print(random_integer)
+ 
