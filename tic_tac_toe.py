56650cda459763333fb1e39860df3dc98ff7fa99
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-03-19 15:53:23 +0000

    Tried to finish the random number generator

---
+++ tic_tac_toe.py
@@ -0,0 +1,11 @@
+# Ryan Crop, Final project
+x = 1 
+x = 3
+O = 0
+import random
+random_integer = random.randint(1, 9)
+if random_integer == x or random_integer == O:
+    random_integer = random.randint
+    print(random_integer)
+else:
+    print ("Hello")
