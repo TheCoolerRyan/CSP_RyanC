6bb2d6879f89d598870fa4e77acb6d80afda0433
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-03-20 15:53:48 +0000

    Worked on tic tac toe

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,12 +1,29 @@
 # Ryan Crop, Final project
-x = 0
-("x") == 52
-("O") == 46
-4 = ("x")
+#grid
+grid = [[1,2,3],
+[4,5,6],
+[7,8,9]]
+
+
 import random
+
+game_finished = False
+
 random_integer = random.randint(1, 9)
-if random_integer == 52 or random_integer == 46:
-    print("Hello")
-else:
-    print(random_integer)
- 
+
+taken_list = []
+
+while game_finished == False:
+
+    if random_integer == taken_list:
+        break
+    else:
+        taken_list.append(random_integer)
+    if random_integer == 1:
+        grid[0][0] = "O" 
+    elif random_integer == 2: #Repeat for numbers one through nine and get updated graph from santiago
+        break
+
+for grid in grid:
+    print(f" {grid[0]} | {grid[1]} | {grid[2]}")
+    #Create a taken list of user inputs so i can  say if random_integer == taken list, break
