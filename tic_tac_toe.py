b7e743aaa55b66b2870599ea26a9a138818b87a3
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-03-20 18:42:21 +0000

    Finished my part.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,4 @@
 # Ryan Crop, Final project
-#grid
 grid = [[1,2,3],
 [4,5,6],
 [7,8,9]]
@@ -9,11 +8,11 @@ import random
 
 game_finished = False
 
-random_integer = random.randint(1, 9)
 
 taken_list = []
 
 while game_finished == False:
+    random_integer = random.randint(1, 9)
 
     if random_integer == taken_list:
         break
@@ -21,9 +20,25 @@ while game_finished == False:
         taken_list.append(random_integer)
     if random_integer == 1:
         grid[0][0] = "O" 
-    elif random_integer == 2: #Repeat for numbers one through nine and get updated graph from santiago
-        break
+    elif random_integer == 2: 
+        grid[0][1] = "O"
+    elif random_integer == 3: 
+        grid[0][2] = "O"
+    elif random_integer == 4: 
+        grid[1][0] = "O"
+    elif random_integer == 5: 
+        grid[1][1] = "O"
+    elif random_integer == 6: 
+        grid[1][2] = "O"
+    elif random_integer == 7: 
+        grid[2][0] = "O"
+    elif random_integer == 8: 
+        grid[2][1] = "O"
+    elif random_integer == 9: 
+        grid[2][2] = "O"
+    break
+
 
 for grid in grid:
-    print(f" {grid[0]} | {grid[1]} | {grid[2]}")
+    print(f" {grid[0]} | {grid[1]} | {grid[2]}\n --+---+--")
     #Create a taken list of user inputs so i can  say if random_integer == taken list, break
