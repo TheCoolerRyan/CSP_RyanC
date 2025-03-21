2cd2870ffb94e0283900a29557df143b977334c2
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-03-21 15:53:31 +0000

    Worked on johanns problem

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -18,25 +18,25 @@ while game_finished == False:
         break
     else:
         taken_list.append(random_integer)
-    if random_integer == 1:
-        grid[0][0] = "O" 
-    elif random_integer == 2: 
-        grid[0][1] = "O"
-    elif random_integer == 3: 
-        grid[0][2] = "O"
-    elif random_integer == 4: 
-        grid[1][0] = "O"
-    elif random_integer == 5: 
-        grid[1][1] = "O"
-    elif random_integer == 6: 
-        grid[1][2] = "O"
-    elif random_integer == 7: 
-        grid[2][0] = "O"
-    elif random_integer == 8: 
-        grid[2][1] = "O"
-    elif random_integer == 9: 
-        grid[2][2] = "O"
-    break
+        if random_integer == 1:
+            grid[0][0] = "O" 
+        elif random_integer == 2: 
+            grid[0][1] = "O"
+        elif random_integer == 3: 
+            grid[0][2] = "O"
+        elif random_integer == 4: 
+            grid[1][0] = "O"
+        elif random_integer == 5: 
+            grid[1][1] = "O"
+        elif random_integer == 6: 
+            grid[1][2] = "O"
+        elif random_integer == 7: 
+            grid[2][0] = "O"
+        elif random_integer == 8: 
+            grid[2][1] = "O"
+        elif random_integer == 9: 
+            grid[2][2] = "O"
+        break
 
 
 for grid in grid:
