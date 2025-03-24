9621eaef75cf8097d52d0b20f89c93b3eca091b9
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-03-24 18:46:45 +0000

    Fixed stuff.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,21 +1,22 @@
 # Ryan Crop, Final project
-grid = [[1,2,3],
-[4,5,6],
-[7,8,9]]
+grid = [['1','2','3'],
+['4','5','6'],
+['7','8','9']]
 
 
 import random
 
 game_finished = False
 
-
+repeat = 1
 taken_list = []
 
 while game_finished == False:
     random_integer = random.randint(1, 9)
 
-    if random_integer == taken_list:
-        break
+    
+    if random_integer in taken_list:
+        continue
     else:
         taken_list.append(random_integer)
         if random_integer == 1:
@@ -36,34 +37,35 @@ while game_finished == False:
             grid[2][1] = "O"
         elif random_integer == 9: 
             grid[2][2] = "O"
-        
-        spot = input("pick a number for your spot 1-9\n !!!SPELL OUT DONT TYPE A NUMBER!!!\n").strip().upper()
+#Show grid after computer goes
+    spot = int(input("pick a number for your spot 1-9 single digit please\n"))
 
     if spot in taken_list:
         print("That spot is taken.")
+        continue
     else: 
         taken_list.append(spot)
-        if spot == "ONE":
+        if spot == 1:
             grid[0][0]="X"
-        elif spot == "TWO":
+        elif spot == 2:
             grid[0][1]  ="X"
-        elif spot == "THREE":
+        elif spot == 3:
             grid[0][2] ="X"
-        elif spot == "FOUR":
+        elif spot == 4:
             grid[1][0]="X"
-        elif spot == "FIVE": 
+        elif spot == 5: 
             grid[1][1]="X"
-        elif spot == "SIX":
+        elif spot == 6:
             grid[1][2] ="X"
-        elif spot == "SEVEN":
+        elif spot == 7:
             grid[2][0] ="X"
-        elif spot == "EIGHT":
+        elif spot == 8:
             grid[2][1] ="X"
-        elif spot == "NINE":
+        elif spot == 9:
             grid[2][2] ="X"
-    break
+    for list in grid:
+        print(f" {list[0]} | {list[1]} | {list[2]}\n --+---+--")
+    grid = grid
 # Figure out how to go back and continue the loop 
 
-for grid in grid:
-    print(f" {grid[0]} | {grid[1]} | {grid[2]}\n --+---+--")
     #Create a taken list of user inputs so i can  say if random_integer == taken list, break
