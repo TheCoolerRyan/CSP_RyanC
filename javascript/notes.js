80101cfa2e69537f89b0bacab37271f60cf2e445
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-04-22 09:53:47 -0600

    Hate java script

---
+++ javascript/notes.js
@@ -0,0 +1,27 @@
+let images = ["https://www.earthdiver.com/cdn-cgi/image/width=4000,quality=75,format=auto/https://assets.earthdiver.com/media/media-2727684.jp?w=5246&h=2193&tick=1667491652923","https://images.squarespace-cdn.com/content/v1/63c60d44c98af85334537583/644359f9-c213-4d77-bdb0-b78b7f8424f3/Mount_Timpanogos_at_sunset.jpg","https://www.nicodebarmore.com/wp-content/uploads/2014/08/wildflower-basin-1024x1020.jpg"]
+document = notes.html
+function hello(){
+    document.getElementById("title").
+    innerHTML = "Hello World!"
+
+}
+count = 0
+function change(){
+    document.getElementById("img").src= images[count]
+    if(count === 2){
+        count = 0
+    }else{
+        count +=1
+    }
+}
+function highlight(){
+    document.getElementById("btn").style.backgroundColor = "orange"
+    document.getElementById("btn").style.color = "white"
+}
+function normal(){
+    document.getElementById("btn").style.backgroundColor="gray"
+    document.getElementById("btn").style.color = "black"
+}
+function show(){
+    document.getElementById("hidden").style.display = "block"
+}
