d518d618ffdcdb1c9143920632e69a56131e1c6e
TheCoolerRyan <ryan.crop@ucas-edu.net>
2025-04-28 09:53:54 -0600

    Fixed so much stuff, all I have to do is write the stuff

--- z_final_stuff/final_project.js
+++ z_final_stuff/final_project.js
@@ -1,6 +1,6 @@
 let images = ["https://th-thumbnailer.cdn-si-edu.com/prCcoKOQiPkBi5QvIyWHEQVIltE=/fit-in/1600x0/https://tf-cmsv2-journeys-media.s3.amazonaws.com/filer/d2/52/d252f665-21c6-4daa-b163-8911ddd0d383/ut_bryce_hoodoos_sunset_ist_176984017.jpg","https://i0.wp.com/www.brycecanyoncountry.com/wp-content/uploads/bryce-canyon-national-park_bestcamping2_5132d5d4.webp","https://www.rubysinn.com/wp-content/uploads/2014/12/8.jpg"]
 
-document = final_project.html
+
 
 let num=0
 
